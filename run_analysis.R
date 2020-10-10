ea67bec9c1b27ef751eadf32a6d754ffe7510186
abitzsong <67314369+abitzsong@users.noreply.github.com>
2020-10-10 22:24:57 +0800

    updates for better understanging

--- run_analysis.R
+++ run_analysis.R
@@ -33,7 +33,7 @@ trainDATA <- cbind(subject_train,y_train,X_train)
 
 #combine two data sets
 DATA <- rbind(testDATA,trainDATA)
-colnames(DATA) <- c("subject","labels",features$activityNames)
+colnames(DATA) <- c("subject","activity",features$activityNames)
 
 
 ##Third part of the script
@@ -41,21 +41,21 @@ colnames(DATA) <- c("subject","labels",features$activityNames)
 select.colnames <- grep("mean\\(\\)|std\\(\\)",colnames(DATA),value = TRUE)
 
 #select columns for DATA
-DATAselected <- select(DATA,subject,labels,select.colnames)
+DATAselected <- select(DATA,subject,activity,select.colnames)
 
 #Uses descriptive activity names to name the activities
 descriptiveLabels <- activity_labels$X2
-DATAselected$labels <- as.factor(DATAselected$labels)
-levels(DATAselected$labels) <- descriptiveLabels
+DATAselected$activity <- as.factor(DATAselected$activity)
+levels(DATAselected$activity) <- descriptiveLabels
 DATAselected <- as_tibble(DATAselected)
 ## DATAselected is the dataset asked for in step 4
 
 ##Forth part of the script
 #creating a tidy dataset with the average of each variable for each activity and each subject
 library(reshape2)
-DATAmelted <- melt(DATAselected,c("labels","subject"),measure.vars = select.colnames, value.name = "value")
-DATAtidy <- dcast(DATAmelted, labels + subject ~ variable, mean)
+DATAmelted <- melt(DATAselected,c("activity","subject"),measure.vars = select.colnames, value.name = "value")
+DATAtidy <- dcast(DATAmelted, subject + activity ~ variable, mean)
 
 #creating a txt tidy dataset for submission
-DATAtidy2 <- arrange(DATAtidy,labels,subject)
+DATAtidy2 <- arrange(DATAtidy,subject,activity)
 write.table(DATAtidy2,file="./tidyData.txt",sep=" ",row.name=FALSE)
